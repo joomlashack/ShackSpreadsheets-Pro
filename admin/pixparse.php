d0d7a8d0a9fdf6f97ba6ff4095036602ca88e78d
johan <johan@986210e8-35e5-42ff-90ab-28d5e4790db6>
2017-09-07 12:21:57 +0000

    Small fixes
    git-svn-id: svn://elias.pixpro.net/joomla30/sites/pixparse/trunk@4975 986210e8-35e5-42ff-90ab-28d5e4790db6

--- admin/pixparse.php
+++ admin/pixparse.php
@@ -10,16 +10,16 @@
 defined('_JEXEC') or die;
 
 // Access check.
-if (!JFactory::getUser()->authorise('core.manage', 'com_pixparse'))
+/*if (!JFactory::getUser()->authorise('core.manage', 'com_pixparse'))
 {
 	throw new Exception(JText::_('JERROR_ALERTNOAUTHOR'));
-}
+}*/
 
 // Include dependancies
-jimport('joomla.application.component.controller');
+jimport( 'joomla.application.component.controller' );
 
-JLoader::registerPrefix('Pixparse', JPATH_COMPONENT_ADMINISTRATOR);
+JLoader::registerPrefix( 'Pixparse', JPATH_COMPONENT_ADMINISTRATOR );
 
-$controller = JControllerLegacy::getInstance('Pixparse', array('base_path' => JPATH_COMPONENT_ADMINISTRATOR));
-$controller->execute(JFactory::getApplication()->input->get('task'));
+$controller = JControllerLegacy::getInstance( 'Pixparse', array( 'base_path' => JPATH_COMPONENT_ADMINISTRATOR ) );
+$controller->execute( JFactory::getApplication()->input->get( 'task' ) );
 $controller->redirect();
