5b35f58d41c067ff1d13567f012b6e4794ed804e
Bill Tomczak <bill@ostraining.com>
2018-09-25 14:26:47 -0700

    Update all the file headers

--- src/script.installer.php
+++ src/script.installer.php
@@ -1,24 +1,26 @@
 <?php
 /**
- * @package   ShackSpreadsheetsPro
- * @contact   www.joomlashack.com, help@joomlashack.com
- * @copyright 2018 Open Source Training, LLC. All rights reserved
- * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ * @package    ShackSpreadsheets-Pro
+ * @contact    www.joomlashack.com, help@joomlashack.com
+ * @author     Johan Sundell <labs@pixpro.net>
+ * @copyright  2003-2017 You Rock AB. All Rights Reserved.
+ * @copyright  2018 Open Source Training, LLC. All rights reserved
+ * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL
  *
- * This file is part of ShackSpreadsheetsPro.
+ * This file is part of ShackSpreadsheets-Pro.
  *
- * ShackSpreadsheetsPro is free software: you can redistribute it and/or modify
+ * ShackSpreadsheets-Pro is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * ShackSpreadsheetsPro is distributed in the hope that it will be useful,
+ * ShackSpreadsheets-Pro is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with ShackSpreadsheetsPro.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ShackSpreadsheets-Pro.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 use Alledia\Installer\AbstractScript;
