893e68ba6c4cd91bd81d250cb0ca9b16c9be59ad
Bill Tomczak <bill@ostraining.com>
2018-09-25 13:09:47 -0700

    Remove files not needed for hidden component

--- src/admin/shackspreadsheets.php
+++ src/admin/shackspreadsheets.php
@@ -7,17 +7,7 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// No direct access
 defined('_JEXEC') or die;
-
-// Access check.
-/*if (!JFactory::getUser()->authorise('core.manage', 'com_shackspreadsheets'))
-{
-	throw new Exception(JText::_('JERROR_ALERTNOAUTHOR'));
-}*/
-
-// Include dependancies
-jimport('joomla.application.component.controller');
 
 JLoader::registerPrefix('Shackspreadsheets', JPATH_COMPONENT_ADMINISTRATOR);
 
